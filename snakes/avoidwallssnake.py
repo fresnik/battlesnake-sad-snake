19a4e20b4b94fcbc22c43ebd0ab244d18322f929
Freyr Bergsteinsson <fresnik@gmail.com>
2020-04-23 23:25:21 +0000

    Fix error in avoidwallssnake

--- snakes/avoidwallssnake.py
+++ snakes/avoidwallssnake.py
@@ -6,10 +6,10 @@ from snakes.utils import get_next_coord, is_coord_on_board
 
 class AvoidWallsSnake(BaseSnake):
     def get_next_move(self, game_state: GameState) -> str:
-        head = game_state['you']['body'][0]
+        head = game_state.data['you']['body'][0]
         for a_dir in all_directions:
             next_head_coord = get_next_coord(head, a_dir)
-            if is_coord_on_board(next_head_coord, game_state['board']):
+            if is_coord_on_board(next_head_coord, game_state.data['board']):
                 return a_dir
 
         return DEFAULT_DIRECTION
