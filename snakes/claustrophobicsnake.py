fa6d324dd555c3d3697db27d1b75d6b827d8c347
Freyr Bergsteinsson <fresnik@gmail.com>
2020-04-24 01:23:08 +0000

    Have ClaustrophobicSnake avoid longer snakes when they get close

--- snakes/claustrophobicsnake.py
+++ snakes/claustrophobicsnake.py
@@ -1,7 +1,9 @@
 from direction import all_directions, DEFAULT_DIRECTION
 from gamestate import GameState
 from snakes.basesnake import BaseSnake
-from snakes.utils import get_next_coord, is_coord_on_board
+from snakes.utils import get_next_coord, get_dist, get_direction, get_opposite_direction
+
+import random
 
 
 class ClaustrophobicSnake(BaseSnake):
@@ -9,8 +11,30 @@ class ClaustrophobicSnake(BaseSnake):
     def get_next_move(self, game_state: GameState) -> str:
         body = game_state.data['you']['body']
         head = body[0]
+
+        # TODO: If we're starving - aim for nearby food
+
+        # If our head is close to the head of another longer snake, try to steer away if possible
+        for snake in game_state.data['board']['snakes']:
+            if snake['id'] == game_state.data['you']['id']:
+                continue
+
+            if len(snake['body']) <= len(game_state.data['you']['body']):
+                continue
+
+            d = get_dist(head, snake['body'][0])
+            if d <= 4:
+                e_direction = get_direction(head, snake['body'][0])
+                e_direction = get_opposite_direction(e_direction)
+                print(f'snake {snake["name"]} is too close and big, trying opposite direction, to the {e_direction}...')
+
+                next_head_coord = get_next_coord(head, e_direction)
+                if next_head_coord not in game_state.snake_body_cells:
+                    return e_direction
+
+        # Move in the direction of the largest area
         largest_area = 0
-        largest_area_direction = DEFAULT_DIRECTION
+        largest_area_directions = []
 
         for a_dir in all_directions:
             next_head_coord = get_next_coord(head, a_dir)
@@ -18,8 +42,11 @@ class ClaustrophobicSnake(BaseSnake):
             size = game_state.get_size_of_empty_cells(next_head_coord)
             print(f'The area is of size {size} if I go {a_dir}...')
 
-            if size > largest_area:
-                largest_area_direction = a_dir
+            if size >= largest_area:
+                largest_area_directions.append(a_dir)
                 largest_area = size
 
-        return largest_area_direction
+        if len(largest_area_directions) == 0:
+            return DEFAULT_DIRECTION
+
+        return random.choice(largest_area_directions)
--- snakes/utils.py
+++ snakes/utils.py
@@ -1,4 +1,5 @@
 import direction
+import math
 
 
 def get_next_coord(curr_coord, a_dir):
@@ -18,3 +19,31 @@ def get_next_coord(curr_coord, a_dir):
 def is_coord_on_board(coord, board) -> bool:
     return 0 <= coord['x'] < board['width'] and \
            0 <= coord['y'] < board['height']
+
+
+def get_dist(coord1, coord2) -> int:
+    return abs(coord2['x'] - coord1['x']) + abs(coord2['y'] - coord1['y'])
+
+
+def get_direction(from_coord, to_coord):
+    vec = (to_coord['x'] - from_coord['x'], to_coord['y'] - from_coord['y'])
+    angle = math.atan2(vec[1], vec[0])
+    if (math.pi / 4) <= angle < (math.pi * 3 / 4):
+        return direction.DOWN
+    elif (math.pi * 3 / 4) <= angle < (math.pi * 5 / 4):
+        return direction.LEFT
+    elif (math.pi * 5 / 4) <= angle < (math.pi * 7 / 4):
+        return direction.UP
+    else:
+        return direction.RIGHT
+
+
+def get_opposite_direction(a_dir):
+    if a_dir == direction.LEFT:
+        return direction.RIGHT
+    elif a_dir == direction.RIGHT:
+        return direction.LEFT
+    elif a_dir == direction.UP:
+        return direction.DOWN
+    else:
+        return direction.UP
